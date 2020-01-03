29371ab79e1c45b882e0f55e7fc4e11117b2214b
bilal waheed <bilal.waheed@arbisoft.com>
2020-01-04 02:42:20 +0500

    first commit

---
+++ test_device_second.py
@@ -0,0 +1,28 @@
+import pytest
+from appium import webdriver
+from TestWebViewAndroid import TestWebViewAndroid 
+
+APPIUM_LOCAL_HOST_URL = 'http://localhost:4723/wd/hub'
+PLATFORM_VERSION = '6.0.1'
+class TestDeviceSecond(TestWebViewAndroid):
+    
+    @pytest.fixture(scope="function")
+    def driver(self, request):
+        desired_caps = {
+            'appPackage': 'com.example.android.contactmanager',
+            'appActivity': '.ContactManager',
+            'platformName': 'Android',
+            'platformVersion': PLATFORM_VERSION,
+            'deviceName': 'Android Emulator',
+            # 'app': PATH('/home/bilal/Documents/ContactManager.apk')
+        }
+        driver = webdriver.Remote(APPIUM_LOCAL_HOST_URL, desired_caps)
+
+        def fin():
+            driver.quit()
+
+        request.addfinalizer(fin)
+        return driver  # provide the fixture value
+
+    def test_add_contacts(self, driver):
+        self.base_test(driver)  
